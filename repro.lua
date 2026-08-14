a554878f63a423310581681afb0bdc1d254c8c6f
Kristian Niemi <kristian.niemi@gmail.com>
2026-08-14 10:25:56 +0200

    fix: surface bootstrap download failures

--- repro.lua
+++ repro.lua
@@ -1,5 +1,8 @@
 vim.env.LAZY_STDPATH = '.repro'
-load(vim.fn.system('curl -s https://raw.githubusercontent.com/folke/lazy.nvim/main/bootstrap.lua'))()
+local bootstrap_url = 'https://raw.githubusercontent.com/folke/lazy.nvim/main/bootstrap.lua'
+local bootstrap = vim.fn.system({ 'curl', '-fsS', bootstrap_url })
+assert(vim.v.shell_error == 0, 'failed to download the lazy.nvim bootstrap: ' .. bootstrap)
+assert(load(bootstrap, 'lazy.nvim bootstrap'), 'downloaded lazy.nvim bootstrap is not valid Lua')()
 
 require('lazy.minit').repro({
   spec = {
